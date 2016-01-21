197221a967566c2e4e7a9984f4f305e68bcdb6e2
NULL-OPERATOR <x@cryptolab.net>
2016-01-21 19:16:41 +0000

    reset temp added, updated powerSave

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -8,33 +8,66 @@ describe('Thermostat', function() {
     thermostat = new Thermostat();
   });
 
-  it('starts at 20 degrees', function() {
-    expect(thermostat.getTemp()).toEqual(20);
-  })
+  describe('thermostat basic functionality', function() {
 
-  it('can increase in temperature', function() {
-    thermostat.upTemp();
-    expect(thermostat.getTemp()).toEqual(21);
-  });
+    it('starts at 20 degrees', function() {
+      expect(thermostat.getTemp()).toEqual(20);
+    })
 
-  it('can decrease in temperature', function() {
-    thermostat.downTemp();
-    expect(thermostat.getTemp()).toEqual(19);
-  });
+    it('can increase in temperature', function() {
+      thermostat.upTemp();
+      expect(thermostat.getTemp()).toEqual(21);
+    });
 
-  it('has a minimun temperature of 5 degrees', function() {
-    for (var i = 0; i < 16; i++) {
+    it('can decrease in temperature', function() {
       thermostat.downTemp();
-    }
-    expect(thermostat.getTemp()).toEqual(5);
+      expect(thermostat.getTemp()).toEqual(19);
+    });
+
+    it('has a minimun temperature of 5 degrees', function() {
+      for (var i = 0; i < 16; i++) {
+        thermostat.downTemp();
+      }
+      expect(thermostat.getTemp()).toEqual(5);
+    });
+
+    it('can reset the temp to 20 degrees', function() {
+      thermostat.upTemp()
+      thermostat.resetTemp()
+      expect(thermostat.getTemp()).toEqual(20);
+    });
   });
 
-  it('power save mode is on by default', function() {
-    expect(thermostat.isPowerSaveOn()).toBe(true);
+  describe('powerSave mode', function() {
+
+    it('power save mode is on by default', function() {
+      expect(thermostat.isPowerSaveOn()).toBe(true);
+    });
+
+    it('powersave can be turned off', function() {
+      thermostat.powerSaveSwitch();
+      expect(thermostat.isPowerSaveOn()).toBe(false);
+    })
   });
 
-  it('powersave can be turned off', function() {
-    thermostat.powerSaveSwitch();
-    expect(thermostat.isPowerSaveOn()).toBe(false);
-  })
+  describe('when poweSave is on', function() {
+
+    it('has a max temp of 26 degrees', function() {
+      for (var i = 0; i < 7; i++) {
+        thermostat.upTemp();
+      }
+      expect(thermostat.getTemp()).toEqual(26);
+    });
+  });
+
+  describe('when poweSave is off', function() {
+
+    it('has a max temp of 36 degrees', function() {
+      thermostat.powerSaveSwitch();
+      for (var i = 0; i < 17; i++) {
+        thermostat.upTemp();
+      }
+      expect(thermostat.getTemp()).toEqual(36);
+    });
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,15 +1,14 @@
 function Thermostat() {
-  this.temperature = 20;
   this.MIN = 5;
+  this.MAX_ON = 26;
+  this.MAX_OFF = 36;
+  this.DEFAULT_TEMP = 20;
+  this.temperature = this.DEFAULT_TEMP;
   this.powerSave = true;
 }
 
 Thermostat.prototype.getTemp = function() {
   return this.temperature;
-};
-
-Thermostat.prototype.upTemp = function() {
-  this.temperature += 1;
 };
 
 Thermostat.prototype.downTemp = function() {
@@ -19,12 +18,31 @@ Thermostat.prototype.downTemp = function() {
   this.temperature -= 1;
 };
 
+Thermostat.prototype.upTemp = function() {
+  if (this._isMaxTemp()) {
+    return;
+  }
+  this.temperature += 1;
+};
+
+Thermostat.prototype.resetTemp = function() {
+  this.temperature = this.DEFAULT_TEMP;
+}
+
 Thermostat.prototype._isMinTemp = function() {
   return this.temperature === this.MIN;
 }
 
+
 Thermostat.prototype.isPowerSaveOn = function() {
   return this.powerSave === true;
+}
+
+Thermostat.prototype._isMaxTemp = function() {
+  if (this.isPowerSaveOn()) {
+    return this.temperature === this.MAX_ON;
+  }
+  return this.temperature === this.MAX_OFF;
 }
 
 Thermostat.prototype.powerSaveSwitch = function() {
